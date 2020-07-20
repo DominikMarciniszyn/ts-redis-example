edf241d079f1d119836e362096563e5b20c8b15c
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-07-20 19:29:30 +0200

    test(general): updated read from cache test

--- tests/cache_client.spec.ts
+++ tests/cache_client.spec.ts
@@ -3,9 +3,11 @@ import CacheClient from '../src/utils/cache_client';
 
 test('Check what is in the cache', async () => {
   const client = new CacheClient(6379, '127.0.0.1');
-  await client.writeToCache('this-is-real-key', 'this-is-real-value');
+  const key = 'this-is-real-key';
+  const value = 'this-is-real-value';
+  await client.writeToCache(key, value);
 
-  const data = await client.readFromCache('test');
+  const data = await client.readFromCache(key);
 
-  await expect(data).toBe('value');
+  await expect(data).toBe(value);
 });
