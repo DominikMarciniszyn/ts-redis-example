fe4f86b51381cf62bc5ed8f9dc72a205a3fda1d7
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-09-17 14:32:48 +0200

    feat: added config for redis

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import { str, num, cleanEnv, CleanEnv } from 'envalid';
+
+
+export const config = cleanEnv(
+  process.env,
+  {
+    REDIS_PORT: num(),
+    REDIS_HOST: str()
+  }
+);
+
+export type AppConfig = Readonly<{
+  REDIS_PORT: number,
+  REDIS_HOST: string
+}> & CleanEnv;
