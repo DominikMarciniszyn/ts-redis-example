681046b3808f0ba3578bf1f8b05f80118a5f8056
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2020-09-17 20:44:44 +0200

    test: write tests for cache client

--- tests/cache_client.spec.ts
+++ tests/cache_client.spec.ts
@@ -2,43 +2,66 @@ import CacheClient from '../src/utils/cache_client';
 
 
 describe('Test Redis Cache Client', () => {
-  const client = new CacheClient(6379, '127.0.0.1');
+  let cache: CacheClient;
 
-  afterAll(async () => {
-    await client.closeConnection();
+  beforeAll(async () => {
+    cache = CacheClient.getInstance();
   });
 
-  test('Test of saving and writing, should return the same value as set', async () => {
+  afterAll(async () => {
+    await cache.closeConnection();
+  });
+
+  it('Test of saving and writing, should return the same value as set', async () => {
     const key = 'this-is-real-key';
     const value = 'this-is-real-value';
-    await client.writeToCache(key, value);
+    await cache.writeToCache(key, value);
 
-    const data = await client.readFromCache(key);
+    const data = await cache.readFromCache(key);
 
     await expect(data).toEqual(value);
   });
 
-  test('Test if given key to read does not exist, should return null', async () => {
+  it('Test if given key to read does not exist, should return null', async () => {
     const key = 'not-exist';
-    const data = await client.readFromCache(key);
+    const data = await cache.readFromCache(key);
 
     await expect(data).toBeNull();
   });
 
-  test('Test if key is inside redis, should return true', async () => {
+  it('Test if key is inside redis, should return true', async () => {
     const key = 'key-to-add';
     const value = 'value-of-key-to-add';
 
-    await client.writeToCache(key, value);
-    const result = await client.isKeyInCache(key);
+    await cache.writeToCache(key, value);
+    const result = await cache.isKeyInCache(key);
 
     await expect(result).toBe(true);
   });
 
-  test('Test if key is inside redis, should return false', async () => {
+  it('Test if key is inside redis, should return false', async () => {
     const key = 'key-which-does-not-exits';
-    const result = await client.isKeyInCache(key);
+    const result = await cache.isKeyInCache(key);
 
     await expect(result).toBe(false);
+  });
+
+  it('Test if key-value was removed from cache', async () => {
+    const key = 'key-to-remove';
+    const value = 'value-of-key-to-remove';
+
+    await cache.writeToCache(key, value, 120);
+    await cache.deleteFromCache(key);
+    const isKeyExists = await cache.isKeyInCache(key);
+
+    await expect(isKeyExists).toBe(false);
+  });
+
+  it('Check the singleton pattern, should be only one Cache client instance', () => {
+    const anotherCache = CacheClient.getInstance();
+
+    expect(cache).toBeInstanceOf(CacheClient);
+    expect(anotherCache).toBeInstanceOf(CacheClient);
+    expect(cache).toBe(anotherCache);
   });
 });
